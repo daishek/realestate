4233ad131f2c4ddcd882150dc6bc62fb6eea58a5
daishek <daichekkal@gmail.com>
2021-12-08 12:21:26 +0100

    Search page, initial search query by default

---
+++ components/SearchFilter.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+import { Flex, Select, Box, Spinner, Input, Text, Button, Icon } from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import Image from "next/image"
+import { MdCancel } from "react-icons/md"
+
+const SearchFilter = () => {
+    return (
+        <div>
+            search...
+        </div>
+    )
+}
+
+export default SearchFilter
--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
-export { default as Property } from './Property';
 export { default as Footer } from './Footer';
+export { default as Property } from './Property';
+export { default as SearchFilter } from './SearchFilter';
