5b29b0a75174a13931c15c1d6cc6472f912ab41f
daishek <daichekkal@gmail.com>
2021-12-09 20:08:57 +0100

    Search filters

--- components/SearchFilter.jsx
+++ components/SearchFilter.jsx
@@ -4,11 +4,43 @@ import { useRouter } from "next/router"
 import Image from "next/image"
 import { MdCancel } from "react-icons/md"
 
+import { filterData, getFilterValues } from "../utils/filterData";
+
 const SearchFilter = () => {
+    const [filters, setFilters] = useState(filterData);
+    console.log(filters);
+    const router = useRouter();
+    //search properties function ...
+    const searchProperties = (filterValues) => {
+        const path = router.pathname;
+        const { query } = router;
+        const values = getFilterValues(filterValues);
+        values.forEach((item)=>{
+            query[item.name] = item.value
+        })
+        console.log(path);
+        router.push({ pathname: path, query })
+    }
     return (
-        <div>
-            search...
-        </div>
+        <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+            {filters.map((filter)=>(
+                <Box key={filter.queryName}>
+                    <Select 
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e)=> searchProperties({ [filter.queryName]:e.target.value }) }
+                    >
+                        {filter?.items?.map((item)=>(
+                            <option 
+                                key={item.value}
+                                value={item.value}
+                            >{item.name}</option>
+                        ))}
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
     )
 }
 
